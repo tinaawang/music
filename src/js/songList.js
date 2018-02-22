4e76cd3e97e372f6957e88b5d866065036b4ccd7
tinaawang <782328816@qq.com>
2018-02-22 11:04:09 +0800

    获取歌曲，点击歌曲激活歌曲项

--- src/js/songList.js
+++ src/js/songList.js
@@ -19,22 +19,53 @@
         },
         deactive(){
             $(this.el).find('.active').removeClass('active')
+        },
+        activeItem(li){
+            let $li = $(li);
+            $li.addClass('active').siblings('.active').removeClass('active');
         }
     }
     let model = {
         data:
             {
                 songs: [ ]
-            }
+            },
+        find(){
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return {id:song.id,...song.attributes}}
+                )
+                return songs;
+            })
+        }
     }
     let controller = {
         init(view,model){
             this.view = view;
             this.model = model;
+            this.bindEvents();
             this.view.render(this.model.data);
+           this.bindEventHub();
+           this.getALLSongs();
+        },
+        getALLSongs(){
+           return  this.model.find().then(() => {
+                this.view.render(this.model.data);
+            });
+        },
+        bindEvents(){
+            $(this.view.el).on('click', 'li', (e) => {
+                this.view.activeItem(e.currentTarget);
+                let songId = e.currentTarget.getAttribute('data-song-id');
+                window.eventHub.emit('select',{id:songId})
+
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload',() => {
                 this.view.deactive();
-                })
+            })
             window.eventHub.on('create',(songData) => {
                 this.model.data.songs.push(songData);
                 this.view.render(this.model.data)
